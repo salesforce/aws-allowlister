c11826570e97e6058b810508e1ceb5e497f8d64e
Kinnaird McQuade <kmcquade@salesforce.com>
2021-01-23 10:31:34 -0500

    More test coverage

--- test/database/test_compliance_data.py
+++ test/database/test_compliance_data.py
@@ -6,14 +6,39 @@ from aws_allowlister.database.database import connect_db
 compliance_data = ComplianceData()
 db_session = connect_db()
 
+
 class ComplianceDataTestCase(unittest.TestCase):
-    def test_get_standard_names(self):
-        result = compliance_data.standard_names(db_session=db_session)
+    def test_standard_names(self):
+        """database.scrapers.compliance_data.ComplianceData.standard_names"""
+        results = compliance_data.standard_names(db_session=db_session)
         # print(result)
-        expected_result = ['SOC', 'PCI', 'ISO', 'FedRAMP', 'HIPAA', 'HITRUST', 'IRAP', 'OSPAR', 'FINMA']
-        self.assertListEqual(result, expected_result)
+        expected_results = ['SOC', 'PCI', 'ISO', 'FedRAMP', 'HIPAA', 'HITRUST', 'IRAP', 'OSPAR', 'FINMA']
+        self.assertListEqual(results, expected_results)
+
+    def test_get_rows(self):
+        """database.scrapers.compliance_data.ComplianceData.get_rows"""
+        results = compliance_data.get_rows(db_session=db_session, service_prefix="s3")
+        print(len(results))
+        print(json.dumps(results, indent=4))
+        expected_results = [
+            {
+                "OSPAR": "true",
+                "IRAP": "true",
+                "HIPAA": "true",
+                "ISO": "true",
+                "SOC": "true",
+                "name": "Amazon S3",
+                "FINMA": "true",
+                "HITRUST": "",
+                "FedRAMP": "",
+                "PCI": "true",
+                "alternative_names": "",
+                "service_prefix": "s3"
+            }
+        ]
 
     def test_get_compliance_status(self):
+        """database.scrapers.compliance_data.ComplianceData.get_compliance_status"""
         # print("getting the status")
         status = compliance_data.get_compliance_status(
             db_session=db_session,
@@ -24,6 +49,7 @@ class ComplianceDataTestCase(unittest.TestCase):
         self.assertEqual(status, True)
 
     def test_get_rows_matching_service_prefix(self):
+        """database.scrapers.compliance_data.ComplianceData.get_rows_matching_service_prefix"""
         results = compliance_data.get_rows_matching_service_prefix(db_session=db_session, service_prefix="s3")
         print(json.dumps(results, indent=4))
         self.assertTrue(len(results) == 1)
@@ -44,3 +70,7 @@ class ComplianceDataTestCase(unittest.TestCase):
             }
         ]
         self.assertTrue(len(results) == 1)
+
+    def test_get_compliant_services(self):
+        """database.scrapers.compliance_data.ComplianceData.get_compliant_services"""
+        results = compliance_data.get_compliant_services(db_session=db_session, compliance_standard="SOC")
