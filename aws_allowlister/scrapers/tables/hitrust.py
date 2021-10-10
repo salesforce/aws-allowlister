49f64c72bd350fbb8be16e236abf569a35f6cef9
HanselD <hansel96@gmail.com>
2021-10-10 21:49:18 +0100

    Added check for status box

--- aws_allowlister/scrapers/tables/hitrust.py
+++ aws_allowlister/scrapers/tables/hitrust.py
@@ -7,7 +7,6 @@ from aws_allowlister.shared.utils import chomp, chomp_keep_single_spaces
 from aws_allowlister.scrapers.common import get_table_ids, clean_status_cell, clean_sdks, get_service_name, clean_status_cell_contents
 from sqlalchemy.orm.session import Session
 
-
 def scrape_hitrust_table(db_session: Session, link: str, destination_folder: str, file_name: str, download: bool = True):
     html_file_path = os.path.join(destination_folder, file_name)
     if os.path.exists(html_file_path):
@@ -49,11 +48,14 @@ def scrape_hitrust_table(db_session: Session, link: str, destination_folder: str
                 # Cell 0: Service name
 
                 this_service_name = get_service_name(cells)
-                
+                # print(f"HITRUST service_name: {this_service_name}")
+
                 # Cell 1: HITRUST CSF checkbox
-                raw_scraping_data.add_entry_to_database(
-                    db_session=db_session,
-                    compliance_standard_name="HITRUST",
-                    sdk="",
-                    service_name=this_service_name,
-                )
+                hitrust_status, hitrust_status_contents = clean_status_cell_contents(cells[1].contents[0])
+                if hitrust_status:
+                    raw_scraping_data.add_entry_to_database(
+                        db_session=db_session,
+                        compliance_standard_name="HITRUST",
+                        sdk="",
+                        service_name=this_service_name,
+                    )
