4db39c833865c16deb9cdcf10dc6d9646e60d3dc
HanselD <hansel96@gmail.com>
2021-10-10 01:46:19 +0100

    fixed typo

--- test/scrapers/test_hitrust_matches.py
+++ test/scrapers/test_hitrust_matches.py
@@ -8,7 +8,7 @@ db_session = connect_db()
 
 class HitrustQATestCase(unittest.TestCase):
     def test_gh_20_HITRUST_compliant_services(self):
-        results = scompliance_data.get_compliant_services(
+        results = compliance_data.get_compliant_services(
             db_session=db_session, compliance_standard="HITRUST"
         )
         expected_results = [
