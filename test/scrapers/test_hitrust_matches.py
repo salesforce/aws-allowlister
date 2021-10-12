178ada808061f220b94e2db870b23a4011fa63a9
HanselD <hansel96@gmail.com>
2021-10-12 21:06:11 +0100

    fixed function name to match github issue number

--- test/scrapers/test_hitrust_matches.py
+++ test/scrapers/test_hitrust_matches.py
@@ -7,7 +7,7 @@ db_session = connect_db()
 
 
 class HitrustQATestCase(unittest.TestCase):
-    def test_gh_20_HITRUST_compliant_services(self):
+    def test_gh_51_HITRUST_compliant_services(self):
         results = compliance_data.get_compliant_services(
             db_session=db_session, compliance_standard="HITRUST"
         )
